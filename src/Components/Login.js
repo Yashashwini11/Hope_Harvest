14d571f526fa3c8ea66530dc939656d71f5745ec
Yashashwini11 <727822tucs258@skct.edu.in>
2024-01-03 10:58:07 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,14 +5,13 @@ import { Link } from 'react-router-dom';
 function Login() {
   return (
     <div className="login-container">
-      <form className="login-form" onSubmit={handleSubmit}>
+      <form className="login-form">
         <h1>Login</h1>
         <div className="form-group">
           <input
             type="email"
             placeholder="Enter your email"
             value={email}
-            onChange={handleEmailChange}
             required
           />
         </div>
@@ -21,7 +20,6 @@ function Login() {
             type="password"
             placeholder="Enter your password"
             value={password}
-            onChange={handlePasswordChange}
             required
           />
         </div>
