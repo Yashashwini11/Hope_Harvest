1844b0799ad005a0b054cd831c9f04eddf718dfc
Yashashwini11 <727822tucs258@skct.edu.in>
2024-01-03 11:25:47 +0530

    update

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -8,29 +8,29 @@ function Description() {
     return (
         <>
        
-        <h1 className='des1'></h1>
+        <h1 className='des1'>.</h1>
             <h1 className='des2'>Causes you can raise funds for</h1>
             <div className="product-grid1">
                 <div className="product1">
                     <div className="zoom1">
-                        <img src={flood} alt="Product Image 1" style={{ height: '190px', width: '190px' }} />
+                        <img src={flood} alt="Product  1" style={{ height: '190px', width: '190px' }} />
                     </div>
                 </div>
                 <div className="product1">
                     <div className="zoom1">
-                        <img src={education} alt="Product Image 2" style={{ height: '190px', width: '190px' }} />
+                        <img src={education} alt="Product  2" style={{ height: '190px', width: '190px' }} />
 
                     </div>
                 </div>
                 <div className="product1">
                     <div className="zoom1">
-                        <img src={women} alt="Product Image 3" style={{ height: '190px', width: '190px' }} />
+                        <img src={women} alt="Product  3" style={{ height: '190px', width: '190px' }} />
 
                     </div>
                 </div>
                 <div className="product1">
                     <div className="zoom1">
-                        <img src={women} alt="Product Image 3" style={{ height: '190px', width: '190px' }} />
+                        <img src={women} alt="Product  3" style={{ height: '190px', width: '190px' }} />
 
                     </div>
                 </div>
--- src/Components/Story.js
+++ src/Components/Story.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../Assets/CSS/Story.css';
-import { Link } from 'react-router-dom';
 
 function Story() {
     return (
